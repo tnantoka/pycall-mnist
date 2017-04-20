6211e17c0412e04505b480d87f4a435ea4ab2e0a
tnantoka <tnantoka@bornneet.com>
2017-04-20 21:35:00 +0900

    Fix app.rb with latest lib

--- app/app.rb
+++ app/app.rb
@@ -11,7 +11,7 @@ end
 
 post '/predict' do
   canvas, pixels = Predictor.parse(params[:data_url])
-  y = predictor.predict(pixels, false)[:y]
+  y = predictor.predict(pixels)
   label = y.argmax.().to_s.to_i
   percent = (y[label] * 100).round(2)
   { label: label, image: canvas.to_data_url, percent: percent }.to_json
